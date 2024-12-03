9aed2c17a74988115e93398157d312daaac8ea6b
Cameron Lovell <cameron.lovell@roboticsystems.com.au>
2024-12-03 18:56:57 +1100

    day 2 done

--- 2024/day-2/src/main.rs
+++ 2024/day-2/src/main.rs
@@ -1,35 +1,30 @@
 use std::fs;
 fn main() {
-    let reports = better_extract_reports("input.txt");
+    let reports = extract_reports("input.txt");
 
     let result = reports.iter().filter(|report| is_safe(report)).count();
-    println!("{result:?}")
+    println!("Part 1: {result:?}");
+
+    let result = reports.iter().filter(|report| is_kina_safe(report)).count();
+    println!("Part 2: {result:?}");
+}
+fn is_kina_safe(report: &[i32]) -> bool {
+    is_safe(report)
+        || (0..report.len()).any(|i| {
+            let mut working = report.to_vec();
+            working.remove(i);
+            is_safe(&working)
+        })
 }
 
-fn is_safe(report: &Vec<i32>) -> bool {
-    for i in 0..report.len() - 1 {
-        let current = report[i];
-        let next = report[i + 1];
-        let diff = next - current;
-
-        if diff.abs() > 3 || diff.abs() < 1 {
-            return false;
-        }
-
-        // This kinda sucks, want to fix it!
-        let direction = get_direction(&report[1], &report[0]);
-        if (diff / diff.abs()) != direction {
-            return false;
-        }
-    }
-    true
+fn is_safe(report: &[i32]) -> bool {
+    let direction = (report[0] - report[1]).signum();
+    report.windows(2).all(|pair| {
+        (1..=3).contains(&(pair[0] - pair[1]).abs()) && (pair[0] - pair[1]).signum() == direction
+    })
 }
 
-fn get_direction(lhs: &i32, rhs: &i32) -> i32 {
-    (lhs - rhs) / (lhs - rhs).abs()
-}
-
-fn better_extract_reports(path: &str) -> Vec<Vec<i32>> {
+fn extract_reports(path: &str) -> Vec<Vec<i32>> {
     fs::read_to_string(path)
         .unwrap()
         .lines()
