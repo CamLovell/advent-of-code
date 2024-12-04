b6058cec67fc2f389586e8baf84a21fdc91cb96d
Cameron Lovell <cameron.lovell@roboticsystems.com.au>
2024-12-04 22:28:29 +1100

    day 4 part 2 done

--- 2024/day-4/src/main.rs
+++ 2024/day-4/src/main.rs
@@ -6,6 +6,45 @@ fn main() {
         word_search.push(line.chars().collect());
     }
 
+    part_1(&word_search);
+    part_2(&word_search);
+}
+fn part_2(word_search: &Vec<Vec<char>>) {
+    let num_rows = word_search.len();
+    let num_cols = word_search[0].len();
+
+    let mut count = 0;
+
+    // Move through word search grid and check everything
+    for row in 0..num_rows - 2 {
+        for col in 0..num_cols - 2 {
+            // check forward diagonal if row and col < len-4
+            let forward: String = word_search
+                .get(row..row + 3)
+                .unwrap()
+                .iter()
+                .enumerate()
+                .map(|(i, column)| column.get(col + i).unwrap())
+                .collect();
+
+            // check backward diagonal if col >= 3 and row < len-4
+            let backward: String = word_search
+                .get(row..row + 3)
+                .unwrap()
+                .iter()
+                .enumerate()
+                .map(|(i, column)| column.get(col + (2 - i)).unwrap())
+                .collect();
+
+            if check_word(&forward, "MAS") && check_word(&backward, "MAS") {
+                count += 1;
+            }
+        }
+    }
+    println!("{count}");
+}
+
+fn part_1(word_search: &Vec<Vec<char>>) {
     let num_rows = word_search.len();
     let num_cols = word_search[0].len();
 
@@ -24,7 +63,7 @@ fn main() {
                     .iter()
                     .collect();
 
-                if check_word(&word) {
+                if check_word(&word, "XMAS") {
                     count += 1;
                 }
             }
@@ -37,7 +76,7 @@ fn main() {
                     .map(|column| column.get(col).unwrap())
                     .collect();
 
-                if check_word(&word) {
+                if check_word(&word, "XMAS") {
                     count += 1;
                 }
             }
@@ -51,7 +90,7 @@ fn main() {
                     .map(|(i, column)| column.get(col + i).unwrap())
                     .collect();
 
-                if check_word(&word) {
+                if check_word(&word, "XMAS") {
                     count += 1;
                 }
             }
@@ -65,7 +104,7 @@ fn main() {
                     .map(|(i, column)| column.get(col - i).unwrap())
                     .collect();
 
-                if check_word(&word) {
+                if check_word(&word, "XMAS") {
                     count += 1;
                 }
             }
@@ -74,6 +113,6 @@ fn main() {
     println!("{count}");
 }
 
-fn check_word(word: &str) -> bool {
-    word == "XMAS" || word == "SAMX"
+fn check_word(word: &str, truth: &str) -> bool {
+    word == truth || word == truth.chars().rev().collect::<String>()
 }
