121d8a482eb8e8fceb253a2b552367a6c6029be2
Cameron Lovell <cameron.lovell@roboticsystems.com.au>
2024-12-09 16:52:06 +1100

    day 5 part 2 done

--- 2024/day-5/src/main.rs
+++ 2024/day-5/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
     let (rule_list, pages_str) = input.split_once("\n\n").unwrap();
 
-    let mut rule_map: HashMap<u32, Vec<u32>> = HashMap::new();
+    let mut rule_map: HashMap<usize, Vec<usize>> = HashMap::new();
     for rule_str in rule_list.lines() {
         let (lhs, rhs) = rule_str.split_once("|").unwrap();
         rule_map
@@ -16,29 +16,51 @@ fn main() {
             .or_insert(vec![lhs.parse().unwrap()]);
     }
 
-    let updates: Vec<Vec<u32>> = pages_str
+    let mut updates: Vec<Vec<usize>> = pages_str
         .lines()
         .map(|line| line.split(",").map(|num| num.parse().unwrap()).collect())
         .collect();
 
-    for up in updates.iter() {
-        if up.len() % 2 == 0 {
-            println!("NO MIDDLE {:?}", up);
-            panic!("");
-        }
-    }
-
-    let part_1: u32 = updates
+    let part_1: usize = updates
         .iter()
         .filter(|update| is_ordered(&update, &rule_map))
         .map(|update| update[update.len() / 2])
         .sum();
 
-    println!("{part_1:?}")
+    println!("{part_1:?}");
+
+    let part_2: usize = updates
+        .iter_mut()
+        .filter(|update| !is_ordered(&update, &rule_map))
+        .map(|update| sort_update(update, &rule_map))
+        .map(|update| update[update.len() / 2])
+        .sum();
+    println!("{part_2:?}");
 }
 
-fn is_ordered(update: &[u32], rule_map: &HashMap<u32, Vec<u32>>) -> bool {
-    let mut bad: HashSet<u32> = HashSet::new();
+fn sort_update<'a>(update: &'a mut [usize], rule_map: &HashMap<usize, Vec<usize>>) -> &'a [usize] {
+    let mut pos = 0;
+    while pos < update.len() {
+        let mut swap_pos: Option<usize> = None;
+        if let Some(rule) = rule_map.get(&update[pos]) {
+            for i in pos + 1..update.len() {
+                if rule.contains(&update[i]) {
+                    swap_pos = Some(i)
+                }
+            }
+        }
+        match swap_pos {
+            Some(idx) => {
+                update.swap(pos, idx);
+            }
+            None => pos += 1,
+        }
+    }
+    update
+}
+
+fn is_ordered(update: &[usize], rule_map: &HashMap<usize, Vec<usize>>) -> bool {
+    let mut bad: HashSet<usize> = HashSet::new();
     for page in update {
         if bad.contains(page) {
             return false;
